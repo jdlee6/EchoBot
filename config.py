f5b18ad64de57e86cdb6ca6a0c508692cf8d1010
Joe <48813806+jdlee6@users.noreply.github.com>
2019-05-10 18:53:37 -0400

    Update and rename config.py to config.json

--- config.py
+++
@@ -1,9 +0,0 @@
-'''
-Configuration: 
-Define Token Key and CMC API Key
-in this file and import those values into
-main.py and cmcAPI.py
-'''
-
-TOKEN_KEY = 'Insert Discord Bot Token here'
-APIkey = 'Insert CMC API Key here'

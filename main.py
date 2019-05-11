e62121d98b7fce0d892b5b714435e615add47295
Joe <48813806+jdlee6@users.noreply.github.com>
2019-05-11 19:29:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ TOKEN = config_data['Keys']['TOKEN_KEY']
 BOT_PREFIX = ['!', '?']
 
 
-description = '''EchoBot is a Discord bot designed to retrieve cryptocurrency data and to convert various currencies into their relative satoshi/gwei values. This program is designed to obtain real time data from Coinmarketcap.com which implements REST APIs. Use either a ! or ? as the command prefix. The creator of this program is "jdlee6" and the source for the bot can be found at: https://github.com/jdlee6/EchoBot'''
+description = '''EchoBot is a Discord bot designed to retrieve cryptocurrency data and to convert various currencies into their relative satoshi/gwei values. This program is designed to obtain real time data from Coinmarketcap.com which implements REST APIs. To use the program type in either a ! or ? as the command prefix followed by the command and the ticker symbol of the coin. The creator of this program is "jdlee6" and the source for the bot can be found at: https://github.com/jdlee6/EchoBot'''
 client = commands.Bot(command_prefix=BOT_PREFIX, description=description)
 
 
